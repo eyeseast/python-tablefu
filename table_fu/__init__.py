1653d2578a9d110a4b0dcfdd1b88ab250ec0013a
Chris Amico <chrisamico@chrisamico-ubuntu.(none)>
2010-04-02 18:09:03 -0400

    initial

---
+++ table_fu/__init__.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+"""
+This is a Python version of Propublica's TableFu Ruby library.
+
+TableFu parses, sorts and formats table-like streams, like CSVs,
+and outputs tables in HTML. It's meant as a utility to make 
+getting tabular on the web easier.
+"""
+import csv
+import urllib2
+
+
+class TableFu(object):
+    pass
+
+
+class Row(object):
+    pass
+
+class Datum(object):
+    pass
